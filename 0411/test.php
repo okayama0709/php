0d2d448519e6c15862018963a37e2959dba522dc
okayu <146696282+okayama0709@users.noreply.github.com>
2024-04-18 16:01:17 +0900

    初コミット

---
+++ 0411/test.php
@@ -0,0 +1,54 @@
+<?php
+echo PHP_INT_MAX;  //定数。呼び出す時はクォートなし
+?>
+
+<br>
+
+<?php
+echo __DIR__; ?>
+
+<br>
+
+<?php
+var_dump("abc"); ?>
+<?php
+echo "<br>";
+var_dump(PHP_INT_MAX * 14);
+echo "<br>";
+?>
+
+<?php
+$client = "山田 洋子";
+echo "ようこそ" . $client . "さん";
+echo "<br>" ?>
+<?php
+echo "<hr>";
+$a = "a";
+$b = "b";
+echo ($a + $b);
+echo ($a . $b);
+echo "<br><br>";
+?>
+
+
+<form action="test.php" method="post">
+    <label for="cm">身長</label>
+    <input type="number" name="cm"><br>
+    <label for="kg">体重</label>
+    <input type="number" name="kg"><br>
+    <input type="submit" value="計算する">
+</form>
+<?php
+
+$kg = ($_POST["kg"]);
+$cm = ($_POST["cm"]);
+$result = ($cm * $cm / 10000) * 22;
+$bmi = $kg / ($cm * $cm / 10000)
+?>
+
+<?php
+echo "<p>あなたの身長は" . $cm . "です</p>";
+echo "<p>あなたの体重は" . $kg . "です</p>";
+echo "<p>平均体重は" . number_format($result, 1) . "です</p>";
+echo "<p>あなたのBMI値は" . number_format($bmi, 1) . "です</p>";
+?>
---
+++ 0418/index.php
@@ -0,0 +1,82 @@
+<pre>
+<!-- 演算子 -->
+<?php
+$a = $true && $false;
+$b = $false && $false;
+$c = $true && $true && $true;
+$d = $true && $false && $false;
+$e = $true && ($true && $false);
+$f = $false && $false;
+
+var_dump($a, $b, $c, $d, $e, $f)
+?>
+
+
+<?php
+$true = true;
+$false = false;
+?>
+</pre>
+<pre>
+<?php
+$g = $true || $false;
+$h = $false || $false;
+$i = $true || $true || $true;
+$j = $true || $false || $false;
+$k = $true || ($true || $false);
+$l = $false || $false;
+
+var_dump($g, $h, $i, $j, $k, $l)
+?>
+
+<pre>
+        <?php
+        $m = !$true;
+        $n = !$false;
+        $o = !$true && !$true;
+        $p = !($true || $true);
+        $q = !($true && $false);
+
+        var_dump($m, $n, $o, $p, $q)
+        ?>
+</pre>
+
+<!-- 制御構分 -->
+
+<?php
+echo time();
+// mktime(時、分、秒、月、日、年)
+$blog = mktime(15, 30, 0, 2, 12, 2024);
+echo $blog . "</br>";
+
+// mktime日付の数値を作る関数
+// time()現在時刻の
+// 86400 : 1日=86400秒
+
+//投稿が１日以上
+// 投稿日時と現在の時刻から１日引く
+// if($blog>$time-86400)
+if ($blog > $time - 86400) {
+    echo "NEW";
+} else {
+    echo "no";
+};
+
+?>
+<br><br>
+<?php
+$age = 23;
+if ($age > 20) {
+    echo "ご購入ありがとうございます";
+} else {
+    echo "購入できません";
+};
+echo "<br>";
+
+if ($age < 20) {
+    echo "購入できません";
+} else if ($age <= 25) {
+    echo "年齢確認が必要です";
+} else {
+    echo "購入できます";
+}
